7da7699a26e08f0b1d06c01defb75138bb5633aa
John Doe <gfscoil@gmail.com>
2015-03-18 01:18:01 +0200

    added audios activity
    added audios activity populated with audios list

--- app/src/main/java/com/ivixor/lowupmania/AudiosListActivity.java
+++ app/src/main/java/com/ivixor/lowupmania/AudiosListActivity.java
@@ -11,6 +11,7 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.util.SimpleArrayMap;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -18,6 +19,7 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -26,6 +28,7 @@ import java.util.List;
 
 public class AudiosListActivity extends ListActivity {
 
+    private List<Song> songs;
     private SongsArrayAdapter mAdapter;
 
     @Override
@@ -33,10 +36,9 @@ public class AudiosListActivity extends ListActivity {
         super.onCreate(savedInstanceState);
 
         DataWrapper dw = (DataWrapper) getIntent().getSerializableExtra("songs");
-        List<Song> songs = dw.getSongsData();
+        songs = dw.getSongsData();
 
-        mAdapter = new SongsArrayAdapter(this,
-                android.R.layout.simple_list_item_1, songs);
+        mAdapter = new SongsArrayAdapter(this, songs);
 
         /*ProgressBar progressBar = new ProgressBar(this);
         progressBar.setLayoutParams(new LinearLayout.LayoutParams(
@@ -66,13 +68,13 @@ public class AudiosListActivity extends ListActivity {
     protected void onListItemClick(ListView l, final View v, int position, long id) {
         //super.onListItemClick(l, v, position, id);
 
-        final String item = (String) l.getItemAtPosition(position);
+        final Song song = (Song) l.getItemAtPosition(position);
         v.animate().setDuration(2000).alpha(0)
                 .withEndAction(new Runnable() {
                     @Override
                     public void run() {
-                        //list.remove(item);
-                        //mAdapter.notifyDataSetChanged();
+                        songs.remove(song);
+                        mAdapter.notifyDataSetChanged();
                         v.setAlpha(1);
                     }
                 });
@@ -82,22 +84,21 @@ public class AudiosListActivity extends ListActivity {
 
         private HashMap<Song, Integer> mIdMap = new HashMap<Song, Integer>();
 
-        public SongsArrayAdapter(Context context, int resourceId, List<Song> objects) {
-            super(context, resourceId, objects);
-            for (int i = 0; i < objects.size(); i++) {
-                mIdMap.put(objects.get(i), i);
+        public SongsArrayAdapter(Context context, List<Song> objects) {
+            super(context, 0, objects);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Song song = getItem(position);
+
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_audio, parent, false);
             }
-        }
 
-        @Override
-        public long getItemId(int position) {
-            Song item = getItem(position);
-            return mIdMap.get(item);
-        }
+            ((TextView) convertView.findViewById(R.id.audioName)).setText(song.getArtist() + " - " + song.getTitle());
 
-        @Override
-        public boolean hasStableIds() {
-            return true;
+            return convertView;
         }
     }
 }
